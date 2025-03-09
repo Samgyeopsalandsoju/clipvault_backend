e4c8780775eb7f37eddd45591ab09c219f3b6a13
Haeseung Lee <dlgomtd93@gmail.com>
2025-03-09 21:34:14 +0900

    feat : 카테고리 생성 최대 갯수 제한

---
+++ src/main/java/com/samso/linkjoa/core/common/commonEnum/CreateLimitEnum.java
@@ -0,0 +1,19 @@
+package com.samso.linkjoa.core.common.commonEnum;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+@Getter
+@RequiredArgsConstructor
+public enum CreateLimitEnum {
+
+    CATEGORY(10),
+    CLIP(100),
+    FORK(20),
+    SHARE(10),
+    OVER(99999)
+    ;
+
+
+    private final long value;
+
+}
