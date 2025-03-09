e71ca9b0bbac79042ae6097c2ed9008c24b18567
Haeseung Lee <dlgomtd93@gmail.com>
2025-03-09 21:53:39 +0900

    fix : 테스트를 위한 count 수정

--- src/main/java/com/samso/linkjoa/core/common/commonEnum/CreateLimitEnum.java
+++ src/main/java/com/samso/linkjoa/core/common/commonEnum/CreateLimitEnum.java
@@ -6,14 +6,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CreateLimitEnum {
 
-    CATEGORY(10),
-    CLIP(100),
-    FORK(20),
-    SHARE(10),
+//    CATEGORY(10),
+//    CLIP(100),
+//    FORK(20),
+//    SHARE(10),
+//    OVER(99999)
+
+    CATEGORY(5),
+    CLIP(5),
+    FORK(5),
+    SHARE(5),
     OVER(99999)
     ;
-
-
     private final long value;
-
 }
