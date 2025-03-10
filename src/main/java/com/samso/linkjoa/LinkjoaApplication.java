cbc52b01a89e7cf25c6dd6f3ae13c143257c8d24
haeseung-lee <dlgotmd93@gmail.com>
2025-03-11 01:23:57 +0900

    fix : 패키지 구조 및 파일 수정

--- src/main/java/com/samso/linkjoa/LinkjoaApplication.java
+++ src/main/java/com/samso/linkjoa/LinkjoaApplication.java
@@ -3,11 +3,13 @@ package com.samso.linkjoa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
+@EnableJpaAuditing
 public class LinkjoaApplication {
 
 	public static void main(String[] args) {
